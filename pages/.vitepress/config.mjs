b2f417b0f1743b746317b9bdf7e1e3c1888e5a9d
Zero2028 <50459628+Zero2028@users.noreply.github.com>
2024-08-28 15:31:31 +0300

    Update config.mjs

--- pages/.vitepress/config.mjs
+++ pages/.vitepress/config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
     nav: [
       { text: 'Главная', link: '/' },
       { text: 'Отзывы', link: '/' },
-      { text: 'Контакты', link: '/Pages/contacts/contacts.md' },
+      { text: 'Контакты', link: '/pages/contacts/contacts.md' },
       { text: 'Благодарности', link: '/' }
     ],
 
