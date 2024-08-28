9be8b775d8ad8884555f2d4cdd9f30a04343f907
VLT <MstrVLT@users.noreply.github.com>
2024-08-28 11:30:06 +0300

    Add files via upload

---
+++ .vitepress/theme/EZLayout.vue
@@ -0,0 +1,62 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-after>
+        <div class="EZInfo">
+            <div style="margin: auto; max-width: 1152px; margin-bottom: 12px;" class="tip custom-block"><p class="custom-block-title">Выберете интересующий Вас раздел</p><p></p></div>
+        </div>
+    </template>
+  </Layout>
+</template>
+
+<style>
+.VPHero.VPHomeHero .container {
+    justify-content: center;
+}
+
+@media (min-width: 960px) {
+    .VPHero.VPHomeHero .container .main {
+        flex-grow: 0 !important;
+        width: auto !important;
+    }
+}
+
+.VPHero.VPHomeHero .container .tagline {
+    text-align: left;
+}
+
+@media (min-width: 960px) {
+    .VPHero.VPHomeHero .container .tagline {
+        text-align: center;
+}
+}
+.EZInfo {
+  position: relative;
+  padding: 0 24px;
+  
+}
+
+@media (min-width: 640px) {
+  .EZInfo {
+    padding: 0 48px;
+  }
+}
+
+@media (min-width: 960px) {
+  .EZInfo {
+    padding: 0 64px;
+    
+  }
+}
+
+article img {
+    aspect-ratio: auto 128 / 128;
+    width: 128px;
+}
+
+</style>
